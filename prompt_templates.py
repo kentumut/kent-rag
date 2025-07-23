785c251780836010667489c0585891d835fa50d3
Kent Umut <kentumut@pm.me>
2025-07-23 09:10:10 +0300

    changed prompt so that RAG answers "he" aswell

--- prompt_templates.py
+++ prompt_templates.py
@@ -5,6 +5,7 @@ rag_prompt = PromptTemplate(
     template=(
         "You are an expert assistant who answers only based on the context below.\n"
         "If the answer is not present, say \"I don't know.\"\n\n"
+        "The context may be written in first person (I, my, me). The question may refer to the same person using 'he', 'his', etc. Match pronouns logically.\n"
         "Context:\n{context}\n\nQuestion:\n{question}\n\nAnswer:"
     ),
 )
